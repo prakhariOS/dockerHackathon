9b7dd8ad5a9174617d29394a90af2a3c5aef012d
prakharg <prakhar@nibejpi.com>
2020-10-10 13:47:55 +0530

    First commit

---
+++ swift/Prakhar/Hackthon_Demo/Hackthon_Demo/ViewController.swift
@@ -0,0 +1,17 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func helloButtonTaped(_ sender: UIButton) {
+        print("Hello")
+    }
+    
+}
+
---
+++ swift/Prakhar/Hackthon_Demo/Hackthon_DemoTests/Hackthon_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Hackthon_DemoTests.swift
+//  Hackthon_DemoTests
+//
+//  Created by Prakhar Gupta on 10/10/20.
+//  Copyright © 2020 demoTesting. All rights reserved.
+//
+
+import XCTest
+@testable import Hackthon_Demo
+
+class Hackthon_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
